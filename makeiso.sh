436157f0a530ab5c1f3ac11d383779dff92d903a
Alex Lev <levzx18@gmail.com>
2023-02-22 17:27:51 +0100

    build: switch to cmake
    The Rust integration in the Makefile was not clean enough. Using Corrosion makes the build system
    cleaner.
    
    BREAKING CHANGE: The previous build system is deleted

--- makeiso.sh
+++ makeiso.sh
@@ -10,13 +10,13 @@ cp limine.cfg iso/limine/limine.cfg
 cp limine/limine.sys iso/limine/limine.sys
 cp limine/limine-cd.bin iso/limine/limine-cd.bin
 cp limine/limine-cd-efi.bin iso/limine/limine-cd-efi.bin
-cp k-rs iso/k-rs
+cp build/krs.elf iso/krs.elf
 
 
 xorriso -as mkisofs -b limine/limine-cd.bin \
         -no-emul-boot -boot-load-size 4 -boot-info-table \
         --efi-boot limine/limine-cd-efi.bin \
         -efi-boot-part --efi-boot-image --protective-msdos-label \
-        iso -o k-rs.iso
+        iso -o krs.iso
 
-./limine/limine-deploy k-rs.iso
+./limine/limine-deploy krs.iso
